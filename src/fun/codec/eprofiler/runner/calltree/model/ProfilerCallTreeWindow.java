41c06e3d5a5b875e1e4cd1dfd161fb7e3b8fbd7f
echo <crazytech@qq.com>
2019-11-23 21:21:51 +0800

    add code jump

--- src/fun/codec/eprofiler/runner/calltree/model/ProfilerCallTreeWindow.java
+++ src/fun/codec/eprofiler/runner/calltree/model/ProfilerCallTreeWindow.java
@@ -4,11 +4,21 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.impl.ToolWindowImpl;
+import com.intellij.pom.Navigatable;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
+import com.intellij.psi.util.ClassUtil;
 import fun.codec.eprofiler.runner.ProfilerCollector;
 import fun.codec.eprofiler.runner.calltree.action.RefreshAction;
 
 import javax.swing.*;
-import javax.swing.event.*;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeExpansionListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
@@ -47,10 +57,34 @@ public class ProfilerCallTreeWindow {
             public void treeExpanded(TreeExpansionEvent event) {
                 //stop print stack tree.
                 project.getComponent(ProfilerCollector.class).print(false);
+
             }
 
             @Override
             public void treeCollapsed(TreeExpansionEvent event) {
+            }
+        });
+        jtree.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                DefaultMutableTreeNode note = (DefaultMutableTreeNode) jtree.getLastSelectedPathComponent();
+                String name = note.toString();
+                String allName = name.substring(0, name.lastIndexOf("("));
+                String className = allName.substring(0, allName.lastIndexOf("."));
+                String methodName = allName.substring(allName.lastIndexOf(".") + 1).trim();
+                PsiClass psiClass = ClassUtil.findPsiClass(PsiManager.getInstance(project), className);
+                if (psiClass != null) {
+                    PsiMethod[] allMethods = psiClass.getAllMethods();
+                    if (allMethods != null) {
+                        for (PsiMethod method : allMethods) {
+                            if (method.getName().equals(methodName)) {
+                                Navigatable navigatable = (Navigatable) method.getNavigationElement();
+                                navigatable.navigate(true);
+                                return;
+                            }
+                        }
+                    }
+                }
             }
         });
     }
